31a735342d00aecd0730a7f8d380926ca1e4c7c0
shsssskn <shsssskn@gmail.com>
2018-04-05 19:47:12 +0900

    [fix] ### scss対応

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
-import { configure } from '@storybook/react';
+import {configure} from '@storybook/react';
+import '../src/styles/index.scss';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
