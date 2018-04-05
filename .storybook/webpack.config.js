c2dbc1c94550106767cc796519546743d583aaa1
shsssskn <shsssskn@gmail.com>
2018-04-05 18:37:13 +0900

    [add] ### storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,65 @@
+// external
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+const path = require('path');
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
+// internal
+const appDirectory = fs.realpathSync(process.cwd());
+const resolvePath = relativePath => path.resolve(appDirectory, relativePath);
+
+// initialize
+const postCSSLoaderOptions = {
+  // 外部CSS読み込みに対応した設定
+  ident: 'postcss',
+  plugins: () => [
+    require('postcss-flexbugs-fixes'),
+    autoprefixer({
+      browsers: ['> 1% in JP'],
+      flexbox: 'no-2009',
+      grid: true,
+    }),
+  ],
+};
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  config.module.rules.push({
+    test: /\.(js|jsx)$/,
+    include: resolvePath('src'),
+    loader: require.resolve('babel-loader'),
+    options: {
+      cacheDirectory: true,
+    },
+  });
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: [
+      require.resolve('style-loader'),
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          importLoaders: 3,
+        },
+      },
+      {
+        loader: require.resolve('resolve-url-loader'),
+      },
+      {
+        loader: require.resolve('sass-loader'),
+        options: {
+          sourceMap: true,
+        },
+      },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: postCSSLoaderOptions,
+      },
+    ],
+  });
+
+  // Return the altered config
+  return config;
+};
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import {Button, Welcome} from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import {Table} from '../src';
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
@@ -17,3 +23,5 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+storiesOf('Table', module).add('Table', () => <Table />);
