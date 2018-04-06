3d5f3282b7768d1688efdcd3807710fc3530a73b
shsssskn <shsssskn@gmail.com>
2018-04-06 22:29:33 +0900

    [update] ### mock table data

--- stories/mock/Table/data.js
+++ stories/mock/Table/data.js
@@ -1,12 +1,12 @@
 export default [
-  {
-    id: '4954835331692',
-    name: 'ｵﾙﾃﾞｨｰﾌﾞ b6-cGG',
-    price: '650 円',
-  },
-  {
-    id: '4954835331692',
-    name: 'ｵﾙﾃﾞｨｰﾌﾞ b6-cGG',
-    price: '650 円',
-  },
+  {id: 1, name: 'りんご', price: 50, prefectures: '青森県'},
+  {id: 2, name: 'みかん', price: 80, prefectures: '愛媛県'},
+  {id: 3, name: 'もも', price: 100, prefectures: '岡山県'},
+  {id: 4, name: 'いちご', price: 70, prefectures: '茨城県'},
+  {id: 5, name: 'バナナ', price: 10, prefectures: '千葉県'},
+  {id: 6, name: 'ぶどう', price: 400, prefectures: '栃木県'},
+  {id: 7, name: '青森りんご', price: 100, prefectures: '青森県'},
+  {id: 8, name: '青りんご', price: 150, prefectures: '岩手県'},
+  {id: 9, name: 'りんご（紅の夢）', price: 999, prefectures: '岩手県'},
+  {id: 10, name: 'りんご（紅玉）', price: 999, prefectures: '秋田県'},
 ];
