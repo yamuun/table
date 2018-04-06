8fda392f22b207b585d995d3785eeb8bb7e61e40
shsssskn <shsssskn@gmail.com>
2018-04-06 12:37:17 +0900

    [add] ### table mock

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,6 +7,9 @@ import {linkTo} from '@storybook/addon-links';
 import {Button, Welcome} from '@storybook/react/demo';
 
 import {Table} from '../src';
+import tableData from './mock/Table/data';
+import tableProps from './mock/Table/props';
+import TableColumns from './mock/TableColumns';
 
 storiesOf('Welcome', module).add('to Storybook', () => (
   <Welcome showApp={linkTo('Button')} />
@@ -24,4 +27,8 @@ storiesOf('Button', module)
     </Button>
   ));
 
-storiesOf('Table', module).add('Table', () => <Table />);
+storiesOf('Table', module)
+  .add('no data', () => <Table />)
+  .add('fruites', () => (
+    <Table data={tableData} columns={TableColumns(tableProps)} />
+  ));
