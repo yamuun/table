881743606aada9a001273c6c5e7c343446c1e4f8
shsssskn <shsssskn@gmail.com>
2019-09-09 16:25:55 +0900

    [update] SCSSではパスエイリアスを使わない

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {Table} from './components/core';
 
+import 'rc-pagination/dist/rc-pagination.min.css';
+import 'react-table/react-table.css';
 import './styles/index.scss';
 
 export {Table};
--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -10,7 +10,9 @@ import {Table} from '../';
 import {TableColumns} from './components/common';
 import data from './data';
 
+import 'rc-pagination/dist/rc-pagination.min.css';
 import '@gemcook/pagination/lib/styles/index.scss';
+import 'react-table/react-table.css';
 import '../styles/index.scss';
 
 storiesOf('Welcome', module).add('to Storybook', () => (
